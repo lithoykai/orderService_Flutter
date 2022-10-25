9a248e6aab6e07fb84642c4f025823522b1bb7d5
leokai <lifetutors.lc@gmail.com>
2022-10-25 19:54:32 -0300

    .

--- lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static const URL_ORDERS =
-      'https://ordensdeservico-4c038-default-rtdb.firebaseio.com/orders';
-  static const URL_ORDER_FINISHED =
-      'https://ordensdeservico-4c038-default-rtdb.firebaseio.com/ordersFinished';
-}
